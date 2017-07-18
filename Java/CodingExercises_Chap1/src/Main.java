9879d5a6aef17bf42d20a0972d30a9fe6b7efdb8
cmansfield <chrismansfield3@hotmail.com>
2017-07-17 19:54:00 -0600

    Added Java exercise 1.4

--- Java/CodingExercises_Chap1/src/Main.java
+++ Java/CodingExercises_Chap1/src/Main.java
@@ -4,8 +4,25 @@ import java.util.function.Function;
 import static org.junit.Assert.*;
 
 
+//iter    Iterate (for each..in)
+//itin    Iterate (for..in)
+//itli    Iterate over a List
+//itar    Iterate elements of array
+//ritar   Iterate elements of array in reverse order
+
+
 public class Main {
 
+    // Note: this exercise shouldn't use the String library
+    private static String replaceAllSpaceWith(char[] chars, final char replaceWith) {
+
+        for (int i=0; i < chars.length; ++i) {
+
+            if (chars[i] == ' ') chars[i] = replaceWith;
+        }
+
+        return new String(chars);
+    }
 
     private static boolean isPermutation(final String a, final String b) {
 
@@ -20,14 +37,14 @@ public class Main {
         return strSort.apply(a).equals(strSort.apply(b));
     }
 
-    public static String reverse(final String str) {
+    private static String reverse(final String str) {
 
         StringBuilder sb = new StringBuilder(str);
 
         return sb.reverse().toString();
     }
 
-    public static boolean hasUniqueChar(final String str) {
+    private static boolean hasUniqueChar(final String str) {
 
         String mutableStr = str;
 
@@ -55,5 +72,13 @@ public class Main {
         // 1.3
         assertTrue(isPermutation("jobs", "sojb"));
         assertFalse(isPermutation("hello", "world"));
+
+        // 1.4
+        assertTrue(
+                replaceAllSpaceWith("Hello my name is John".toCharArray(), '5')
+                        .equals("Hello5my5name5is5John"));
+
+        // 1.5
+        
     }
 }
